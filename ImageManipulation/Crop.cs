78a3abdc8d94a42b64be7a262d1282c3cb74088d
Martin Kearn <martin.kearn@microsoft.com>
2018-07-31 14:04:32 +0100

    Code tidy up

--- ImageManipulation/Crop.cs
+++ ImageManipulation/Crop.cs
@@ -1,67 +1,64 @@
 
-using System.IO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
-using System.Net.Http;
+using System;
 using System.Drawing;
-using System.Drawing.Imaging;
+using System.IO;
 
 namespace ImageManipulation
 {
     public static class Crop
     {
         [FunctionName("Crop")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            log.Info("Crop function processed a request.");
 
-            var x = 100;
-            var y = 100;
-            var width = 400;
-            var height = 400;
+            // Get params from request. Default to 10x10px square from top left corner. If the source images is smaller than 10x10, this will throw an exception; sorry about that but this is just a sample ;)
+            var parameters = req.GetQueryParameterDictionary();
+            var y = (parameters.ContainsKey("y")) ? Convert.ToInt16(parameters["y"]) : 0;
+            var x = (parameters.ContainsKey("x")) ? Convert.ToInt16(parameters["x"]) : 0;
+            var width = (parameters.ContainsKey("width")) ? Convert.ToInt16(parameters["width"]) : 10;
+            var height = (parameters.ContainsKey("height")) ? Convert.ToInt16(parameters["height"]) : 10;
 
-            Image sourceImage = null;
-            Image destinationImage = null;
-
-            // resize image
-            byte[] bytes;
-            using (var streamReader = new MemoryStream())
+            // Resize image and save to destination Image
+            Image sourceImage;
+            Image destinationImage;
+            using (var ms = new MemoryStream())
             {
-                req.Body.CopyTo(streamReader);
-                bytes = streamReader.ToArray();
+                // Copy body of request to the memory stream
+                req.Body.CopyTo(ms);
 
-                using (Bitmap temp = new Bitmap(streamReader))
-                {
-                    sourceImage = new Bitmap(temp);
+                // Copy the contents of memory stream to the source image file
+                sourceImage = new Bitmap(ms);
 
-                    destinationImage = new Bitmap(width, height);
-                    Graphics g = Graphics.FromImage(destinationImage);
+                // Create a new destination image of the required size
+                destinationImage = new Bitmap(width, height);
 
-                    g.DrawImage(
-                      sourceImage,
-                      new Rectangle(x, y, width, height),
-                      new Rectangle(x, y, width, height),
-                      GraphicsUnit.Pixel
-                    );
-                }
+                // Draw the destination image from the source based on bounding box 
+                var boundingBox = new Rectangle(x, y, width, height);
+                Graphics g = Graphics.FromImage(destinationImage);
+                g.DrawImage(sourceImage, boundingBox, boundingBox,GraphicsUnit.Pixel);
             }
 
-            // write to output byte array
+            // Write destination image to byte array
             byte[] outputBytes = null;
             using (var ms = new MemoryStream())
             {
+                // Save image to memory stream
                 destinationImage.Save(ms, destinationImage.RawFormat);
+
+                // Save memory stream to byte array
                 outputBytes = ms.ToArray();
             }
 
-            // good tool for testing/visulaising the output byte array https://codebeautify.org/base64-to-image-converter
+            // NOTE: There is a good tool for testing the output byte array https://codebeautify.org/base64-to-image-converter
 
-
-            return (ActionResult)new OkObjectResult(outputBytes);
+            // Return a byte array wrapped in an OK result
+            return new OkObjectResult(outputBytes);
         }
 
 
