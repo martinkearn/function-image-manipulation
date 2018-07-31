38e5169bc7643b2766478fdb982b97082595c4b7
Martin Kearn <martin.kearn@microsoft.com>
2018-07-31 11:17:27 +0100

    Using system.drawing

--- ImageManipulation/Crop.cs
+++ ImageManipulation/Crop.cs
@@ -6,6 +6,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ImageManipulation
 {
@@ -18,19 +21,49 @@ namespace ImageManipulation
 
             string name = req.Query["name"];
 
+            var x = 0;
+            var y = 0;
+            var width = 50;
+            var height = 50;
+
+
+            Image sourceImage = null;
+            Image destinationImage = null;
+
             // get byte array from body
             byte[] bytes;
             using (var streamReader = new MemoryStream())
             {
                 req.Body.CopyTo(streamReader);
                 bytes = streamReader.ToArray();
+
+                using (Bitmap temp = new Bitmap(streamReader))
+                {
+                    sourceImage = new Bitmap(temp);
+
+                    destinationImage = new Bitmap(width, height);
+                    Graphics g = Graphics.FromImage(destinationImage);
+
+                    g.DrawImage(
+                      sourceImage,
+                      new Rectangle(x, y, width, height),
+                      new Rectangle(x, y, width, height),
+                      GraphicsUnit.Pixel
+                    );
+                }
             }
 
-            //Crop image
+            byte[] outputBytes = null;
+            using (var ms = new MemoryStream())
+            {
+                destinationImage.Save(ms, destinationImage.RawFormat);
+                outputBytes = ms.ToArray();
+            }
 
 
-
-            return (ActionResult)new OkObjectResult($"OK");
+            return (ActionResult)new OkObjectResult(outputBytes);
         }
+
+
     }
 }
